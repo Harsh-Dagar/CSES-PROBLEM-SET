b2b76dbda1ae0a6ab108d4ec92962b119a898f32
HarshDagar <harshdagar8181@gmail.com>
2026-08-12 01:38:10 +0530

    counting towers(optimize lil bit)

--- DP/CountingTowers.java
+++ DP/CountingTowers.java
@@ -1,35 +1,43 @@
-// package DP;
-import java.util.*;
+package DP;
 
+import java.util.*;
 
 public class CountingTowers {
 
-    static final int MOD = (int)1e9+7;
+    static final int MOD = (int) 1e9 + 7;
+    static long dp[][];
 
-    static int solve(int n){
-        long dp[][] = new long[n+1][2];
-        dp[1][0]=1;
-        dp[1][1]=1;
-        //0->split
-        //1->not split
-        for(int i = 2;i<=n;i++){
-            //non split case 
-            dp[i][1] = ((2*dp[i-1][1])+(dp[i-1][0]))%MOD;
-            //split case
-            dp[i][0] = ((dp[i-1][1])+(4*dp[i-1][0]))%MOD;
+    static void populateDP(int n) {
+        dp[1][0] = 1;
+        dp[1][1] = 1;
+        // 0->split
+        // 1->not split
+        for (int i = 2; i <= n; i++) {
+            // non split case
+            dp[i][1] = ((2 * dp[i - 1][1]) + (dp[i - 1][0])) % MOD;
+            // split case
+            dp[i][0] = ((dp[i - 1][1]) + (4 * dp[i - 1][0])) % MOD;
         }
-        return (int)(dp[n][0]+dp[n][1])%MOD;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        while(t>0){
-            int n = sc.nextInt();
-            int res = solve(n);
+        int inputs[] = new int[t];
+        int maxNum = 0;
+        for (int i = 0; i < t; i++) {
+            inputs[i] = sc.nextInt();
+            maxNum = Math.max(maxNum, inputs[i]);
+        }
+        dp = new long[maxNum + 1][2];
+        populateDP(maxNum);
+
+        for(int i =0;i<t;i++){
+            int currN = inputs[i];
+            long res = (dp[currN][0]+dp[currN][1])%MOD;
             System.out.println(res);
-            t=t-1;
         }
         sc.close();
     }
-    
+
 }
